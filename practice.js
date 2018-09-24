bd0f23c8d964420da8d33068d2ab429360f7c1c8
tstrat <tstrat@email.arizona.edu>
2018-09-23 20:11:25 -0700

    probelm 1, complete

--- practice.js
+++ practice.js
@@ -6,7 +6,9 @@
 //plus the value of the name parameter.
 
 //Code here
-
+function greeting(name) {
+  return `Hello, ${name}`;
+}
 //////////////////PROBLEM 2////////////////////
 
 //Rewrite the function greeting as a function expression.
